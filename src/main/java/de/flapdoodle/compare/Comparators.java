a4921f026ea5e749a8605515edc261436505feb5
Michael Mosmann <extern.mmosmann@novomind.com>
2017-07-13 15:36:00 +0200

    simplified

--- src/main/java/de/flapdoodle/compare/Comparators.java
+++ src/main/java/de/flapdoodle/compare/Comparators.java
@@ -6,11 +6,20 @@ import java.util.function.Function;
 
 public class Comparators {
 	
-	public static <S,D> ComparatorMatcher<S> matcher(Function<S, Optional<D>> mapper, Comparator<D> comparator) {
-		return (left,right) -> {
-			return mapper.apply(left)
-					.flatMap(l -> mapper.apply(right)
-							.map(r -> () -> comparator.compare(l, r)));
+	public static <S,D> Comparator<S> matching(Function<S, Optional<D>> mapper, Comparator<D> comparator) {
+		return (left, right) -> {
+			Optional<D> matchLeft = mapper.apply(left);
+			Optional<D> matchRight = mapper.apply(right);
+			if (matchLeft.isPresent() && matchRight.isPresent()) {
+				return comparator.compare(matchLeft.get(), matchRight.get());
+			}
+			if (matchLeft.isPresent()) {
+				return -1;
+			}
+			if (matchRight.isPresent()) {
+				return 1;
+			}
+			return 0;
 		};
 	}
 }
--- src/test/java/de/flapdoodle/compare/ComparatorsTest.java
+++ src/test/java/de/flapdoodle/compare/ComparatorsTest.java
@@ -3,7 +3,10 @@ package de.flapdoodle.compare;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Comparator;
-import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,11 +17,10 @@ import de.flapdoodle.types.Types;
 public class ComparatorsTest {
 
 	@Test
-	public void simpleTest() {
-		Comparator<Object> comparator = Comparators.matcher(Types.ifInstance(String.class), String::compareTo)
-			.or(Comparators.matcher(Types.ifInstance(Integer.class), Integer::compareTo))
-			.or(Comparators.matcher(Types.ifInstance(Long.class), Long::compareTo))
-			.asComparator();
+	public void simpleSample() {
+		Comparator<Object> comparator = Comparators.matching(Types.ifInstance(String.class), String::compareTo)
+			.thenComparing(Comparators.matching(Types.ifInstance(Integer.class), Integer::compareTo))
+			.thenComparing(Comparators.matching(Types.ifInstance(Long.class), Long::compareTo));
 		
 		String result = Stream.of(0L, 0,"foo",12,"bar",1,12L,3.0)
 			.sorted(comparator)
@@ -28,4 +30,37 @@ public class ComparatorsTest {
 		assertEquals("bar(String), foo(String), 0(Integer), 1(Integer), 12(Integer), 0(Long), 12(Long), 3.0(Double)", result);
 	}
 
+	@Test
+	public void advanceSample() {
+		Comparator<String> comparator = Comparators.matching(matchNumber("(?<number>[0-9]+) m", "number"), Integer::compareTo)
+			.thenComparing(Comparators.matching(matchNumber("(?<number>[0-9]+) L", "number"), Integer::compareTo))
+			.thenComparing(Comparators.matching(matchRegex("(?<all>[0-9]+)", "all"), String::compareTo));
+		
+		String result = Stream.of("1","2","20 m","100 m","14 L","3 L")
+			.sorted(comparator)
+			.collect(Collectors.joining(", "));
+		
+		assertEquals("20 m, 100 m, 3 L, 14 L, 1, 2", result);
+	}
+
+	public static <S,OA,OE> Function<S, Optional<OE>> onResult(Function<S, Optional<OA>> src, Function<OA, OE> mapper) {
+		return src.andThen(os -> os.map(mapper));
+	}
+	
+	private static Function<String, Optional<Integer>> matchNumber(String regex, String groupName) {
+		return onResult(matchRegex(regex, groupName), Integer::valueOf);
+	}
+	
+	private static Function<String, Optional<String>> matchRegex(String regex, String groupName) {
+		return matchRegex(Pattern.compile(regex), groupName);
+	}
+	
+	private static Function<String, Optional<String>> matchRegex(Pattern pattern,String groupName) {
+		return s -> {
+			Matcher matcher = pattern.matcher(s);
+			return matcher.matches() 
+					? Optional.of(matcher.group(groupName)) 
+					: Optional.empty();
+		};
+	}
 }
